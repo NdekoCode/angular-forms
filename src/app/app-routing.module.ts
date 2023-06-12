5d207a3ecca355e6a5cbe505c0b8e8b83fbe78a1
NdekoCode <arickbulakali@gmail.com>
2023-06-12 11:20:10 +0200

    feat: add login form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from 'src/app/shared/components/form/form.component';
+import { LoginFormComponent } from './shared/components/login-form/login-form.component';
 import { RegisterComponent } from './shared/components/register/register.component';
 
 const routes: Routes = [
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'form',
     component: FormComponent,
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './shared/components/form/form.component';
 import { RegisterComponent } from './shared/components/register/register.component';
+import { LoginFormComponent } from './shared/components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [AppComponent, FormComponent, RegisterComponent],
+  declarations: [AppComponent, FormComponent, RegisterComponent, LoginFormComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
